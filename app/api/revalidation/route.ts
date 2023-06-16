0b36f750ebc2e54e0732ca8d75ed743128a41221
Kasper Sprengers <k.sprengers@korsit.com>
2023-06-16 12:45:10 +0000

    revalidate page

--- app/api/revalidation/route.ts
+++ app/api/revalidation/route.ts
@@ -5,6 +5,7 @@ export const runtime = 'edge';
 
 export async function GET(request: NextRequest) {
     revalidateTag("pokemon")
+    revalidateTag("/pokemon/page")
     // revalidatePath('/[locale]/brand/[slug]');
 
     return NextResponse.json({ revalidated: true, now: Date.now() })
