ffa307717d3116bd755db3b5017375f132cef7c1
Kasper Sprengers <k.sprengers@korsit.com>
2023-06-16 12:40:11 +0000

    .

--- app/[locale]/pokemon/page.tsx
+++ app/[locale]/pokemon/page.tsx
@@ -1,19 +1,7 @@
-function randomIntFromInterval(min: number, max: number) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-async function getPokemonItem() {
-    const milli = new Date().getMilliseconds();
-    const number = milli % 3 || 1;
-    console.log("number", number)
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomIntFromInterval(1, 10)}`, {
-        next: {
-            tags: ["pokemon"]
-        }
-    });
-    return await pokemon.json()
-}
+import { getRandomPokemonItem } from "@/services/pokemonService";
+
 export default async function RandomPokemon () {
-    const pokemon = await getPokemonItem();
+    const pokemon = await getRandomPokemonItem();
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
---
+++ services/pokemonService.ts
@@ -0,0 +1,11 @@
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+export async function getRandomPokemonItem() {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomIntFromInterval(1, 1000)}`, {
+        next: {
+            tags: ["pokemon"]
+        }
+    });
+    return await pokemon.json()
+}
