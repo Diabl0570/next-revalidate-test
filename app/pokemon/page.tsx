11434250e7ccfb134929adba0a9b1220502a34bb
Kasper <Diabl0570@users.noreply.github.com>
2023-06-16 15:40:35 +0200

    add redirect (#2)
    * add redirect
    
    * add client component to revalidate
    
    * get and post equal
    
    * add suspence
    
    * force static
    
    * error
    
    * revalidate link
    
    * typo
    
    * target blank
    
    ---------
    
    Co-authored-by: Kasper Sprengers <k.sprengers@korsit.com>

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -1,12 +1,16 @@
 import { getRandomPokemonItem } from "@/services/pokemonService";
 
-export default async function RandomPokemon () {
+export default async function RandomPokemon() {
     const pokemon = await getRandomPokemonItem();
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="relative flex place-items-center ">
                 <h1>{pokemon.name}: {pokemon.id}</h1>
+                <p>
+                    <br />
+                    <a href="/api/revalidation" target="__BLANK">Revalidate link</a>
+                </p>
             </div>
         </main>
     )
