383ee56ae69c5d64842d50eccbab4aade0d8ed76
Kasper Sprengers <k.sprengers@korsit.com>
2023-06-16 13:15:54 +0000

    .

--- app/pokemon/func.ts
+++ app/pokemon/func.ts
@@ -4,7 +4,6 @@ export async function triggerRevalidate() {
     const url = process.env.VERCEL_URL || "http://localhost:3000";
     const result = await fetch(`${url}/api/revalidation`, {
         method: "post",
-        cache: "no-cache"
     })
 
     return await result.json()
